412cf5d0d9ffe30dd2e05ba61c658353f08188c3
allcallsmeprakash <154727203+allcallsmeprakash@users.noreply.github.com>
2025-07-18 15:53:50 +0530

    Create main.tf

---
+++ terraform/environments/dev/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.2"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+module "lambda" {
+  source      = "./modules/lambda"
+  lambda_name = var.lambda_name
+  image_uri   = var.image_uri
+}
+
+module "apigateway" {
+  source             = "./modules/apigateway"
+  api_name           = var.api_name
+  region             = var.aws_region
+  lambda_invoke_arn  = module.lambda.lambda_arn
+  lambda_name        = module.lambda.lambda_name
+  stage_name         = var.stage_name
+}
