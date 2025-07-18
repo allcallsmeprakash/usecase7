88a5d08d1e38d4e4af0489cc9e23b7ad3935b7d9
allcallsmeprakash <154727203+allcallsmeprakash@users.noreply.github.com>
2025-07-18 16:12:05 +0530

    Update and rename main.tf to main.tf

---
+++ terraform/modules/iam-lambda/main.tf
@@ -0,0 +1,36 @@
+resource "aws_iam_role" "lambda_exec" {
+  name = "${var.lambda_name}_role"
+  assume_role_policy = data.aws_iam_policy_document.lambda_assume_role.json
+}
+
+data "aws_iam_policy_document" "lambda_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_xray" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSXRayDaemonWriteAccess"
+}
+
+resource "aws_lambda_function" "this" {
+  function_name = var.lambda_name
+  package_type  = "Image"
+  image_uri     = var.image_uri
+  role          = aws_iam_role.lambda_exec.arn
+  timeout       = 10
+
+  tracing_config {
+    mode = "Active"
+  }
+}
